dd08bbbe46a1dee8854b60c4b37ab3bc81385de2
Felix Dietze <felix@beerleader.net>
2013-02-25 19:01:47 +0100

    introducing obstacles

--- src/main/scala/playgod/CreatureSimulation.scala
+++ src/main/scala/playgod/CreatureSimulation.scala
@@ -12,7 +12,9 @@ class CreatureSimulation(
   val world = new World(new Vec2(0, -9.81f), true)
   world.setDebugDraw(DebugDrawer)
 
-  val ground = createBox(world, new Vec2(0, -3), hx = 500, hy = 3, density = 0f, friction = 1f)
+  val ground = createBox(world, pos = new Vec2(0, -3), hx = 500, hy = 3, density = 0f, friction = 1f)
+  for( i <- 0 until 20 )
+    createBox(world, pos = new Vec2(30 + i*10f, -3), hx = 2.5f, hy = 3f + i / 10f, density = 0f, friction = 1f)
   val creature = creatureDef.create(world, brainWeights)
 
   override def update() {
